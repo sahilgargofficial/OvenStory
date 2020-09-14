1ad9b5dca49ae523ef990f311907f04b4b5f61b5
Sahil Kumar <sahilgargofficial@gmail.com>
2020-09-14 21:24:13 +0530

    add cart and auth pages

--- server.js
+++ server.js
@@ -5,15 +5,25 @@ const path = require('path')
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 // set templating engine
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(PORT, () => {
     console.log(`app deployed on port ${PORT}`);
