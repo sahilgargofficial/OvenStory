e7d70ce001544f31ef8909a7174c61b7f5dfec01
Sahil Kumar <sahilgargofficial@gmail.com>
2020-09-16 11:15:40 +0530

    policy break warning resolver

--- routes/web.js
+++ routes/web.js
@@ -16,5 +16,3 @@ function initRoutes(app) {
 }
 
 module.exports = initRoutes;
-
-/* mongodb+srv://sahilgargofficial:Y76daEMxuJ6gkta4@nature-instance-a0eqz.mongodb.net/nature?authSource=admin&replicaSet=nature-instance-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true */
